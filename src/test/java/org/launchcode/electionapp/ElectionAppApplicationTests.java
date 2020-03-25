70eca3afc365a50a166f5972f89addef31d1283f
Kyle <kratclif22@gmail.com>
2020-03-25 18:00:32 -0500

    Initial commit

---
+++ src/test/java/org/launchcode/electionapp/ElectionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.electionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
